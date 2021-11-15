b98ff0a7c6ff67b0bbca2932040a8783a0b0ba2b
clementbidan <clement.bidan@centrale-marseille.fr>
2021-11-15 12:02:13 +0100

    comma issue

--- components/MondialRelayShippingPrices.js
+++ components/MondialRelayShippingPrices.js
@@ -1,4 +1,5 @@
 export function get_mondial_relay_price(poids) {
+  poids = poids.replace(",", ".");
   const prices = [4.4, 4.9, 6.3, 6.5, 6.9, 9.9, 11.9, 13.5, 17.9, 19.9, 24.9];
   const ranges = [0.5, 1, 2, 3, 4, 5, 7, 10, 15, 20, 30];
   if (Number(poids) <= 0.5) {
